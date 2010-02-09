fd3da1ad272a2765c906f2ff186bcaf98b7d0e20
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-02-09 14:15:36 +0000

    Removed adbrite + Author Notes in footer

--- tulip_group/footer.php
+++ tulip_group/footer.php
@@ -1,8 +1,4 @@
-<div class="search"><form method="get" id="searchform" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-
-<input type="text"  name="s" id="s" class="form" value="<?php echo wp_specialchars($s, 1); ?>"/><input type="image" src="<?php bloginfo('stylesheet_directory'); ?>/images/search.gif" value="submit" style="border:0; margin:0; vertical-align:top"/></form>
-
-</div><div id="footer"><br><?php bloginfo('name'); ?> runs on <a href="http://www.wordpress.org">WordPress</a> and <a href="http://www.writerspace.net">Digital Pop</a> theme - a <a href="http://www.hellowiki.com">NewFeel</a> mod.
+<div id="footer"><br><?php bloginfo('name'); ?> runs on <a href="http://www.wordpress.org">WordPress</a>.
 
 <?php wp_footer(); ?>
 
