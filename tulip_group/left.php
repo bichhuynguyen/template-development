a2aff8d7aca8b89753534f046bb28748e6e7bc80
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-02-09 14:24:30 +0000

    Removed Twitter/Google Reader Added Sign in Page

--- tulip_group/left.php
+++ tulip_group/left.php
@@ -6,6 +6,25 @@
 
 <a href="<?php echo get_settings('home'); ?>/" title="<?php bloginfo('name'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo.jpg" alt="<?php bloginfo('name'); ?>" border="0" /></a>
 
+<!--Login Script Start-->
+<?php if (!(current_user_can('level_0'))){ ?>
+<h2>Login</h2>
+<form action="<?php echo get_option('home'); ?>/wp-login.php" method="post">
+<input type="text" name="log" id="log" value="<?php echo wp_specialchars(stripslashes($user_login), 1) ?>" size="20" />
+<input type="password" name="pwd" id="pwd" size="20" />
+<input type="submit" name="submit" value="Send" class="button" />
+    <p>
+       <label for="rememberme"><input name="rememberme" id="rememberme" type="checkbox" checked="checked" value="forever" /> Remember me</label>
+       <input type="hidden" name="redirect_to" value="<?php echo $_SERVER['REQUEST_URI']; ?>" />
+    </p>
+</form>
+<a href="<?php echo get_option('home'); ?>/wp-login.php?action=lostpassword">Recover password</a>
+<?php } else { ?>
+<h2>Logout</h2>
+<a href="<?php echo wp_logout_url(urlencode($_SERVER['REQUEST_URI'])); ?>">logout</a><br />
+<a href="http://XXX/wp-admin/">admin</a>
+<?php }?>
+<!--Login Script End-->
 
 <?php if (function_exists('blc_latest_comments')) { ?>
 
@@ -15,8 +34,6 @@
 
 <?php } ?> 
 
-<a href="http://getfirefox.com" title="Get Firefox"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/firefox.gif" alt="Get Firefox" border="0" /></a>
-<a href="<?php bloginfo('rss2_url'); ?>" title="Subscribe to my Feed" rel="alternate" type="application/rss+xml"><img alt="Subscribe to my feed" style="border:0" src="<?php bloginfo('stylesheet_directory'); ?>/images/feed.gif"/></a>
-<br>
-<a href="http://twitter.com/YOUR_TWITTER_FEED" title="Subscribe to my Twitter Feed" rel="alternate" type="application/rss+xml"><img alt="Subscribe to my Twitter feed" style="border:0" src="<?php bloginfo('stylesheet_directory'); ?>/images/twitterfeed.gif"/></a>
+
+
 </div>
