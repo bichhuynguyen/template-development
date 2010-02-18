8eb63d16e7922bc3413d41ddbeda5374d307bbe2
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-02-18 13:48:08 +0000

    Init

---
+++ trunk/muslim_youth/footer.php
@@ -0,0 +1,5 @@
+<div id="footer"><br><?php bloginfo('name'); ?> runs on <a href="http://www.wordpress.org">WordPress</a>.
+
+<?php wp_footer(); ?>
+
+</div>
---
+++ trunk/muslim_youth/sidebar.php
@@ -0,0 +1,19 @@
+<div id="sidebar">
+
+<h2>Pages</h2>
+<ul>
+<li><a href="<?php echo get_settings('home'); ?>/">Home</a></li>
+<?php wp_list_pages('title_li='); ?> 
+</ul>
+
+<h2>Categories</h2>
+<ul>
+<?php wp_list_cats('arguments'); ?>
+</ul>
+
+<!--><h2>Blogroll</h2>
+<ul>
+<?php get_links(); ?>
+</ul>-->
+
+</div>
