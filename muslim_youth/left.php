42f4cc18de5216819751024173db079c6abf9ad6
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-02-18 16:05:34 +0000

    Shift to Full Yellow Black Style

--- muslim_youth/left.php
+++ muslim_youth/left.php
@@ -1,15 +1,10 @@
 <div id="left">
 
-<a href="<?php echo get_settings('home'); ?>/" title="<?php bloginfo('name'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo.png" alt="<?php bloginfo('name'); ?>" border="0" /></a>
-
+<!--><a href="<?php echo get_settings('home'); ?>/" title="<?php bloginfo('name'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo.png" alt="<?php bloginfo('name'); ?>" border="0" /></a> -->
 <ul class="latest">
-<?php wp_get_archives('type=postbypost&limit=9'); ?>
+<?php wp_get_archives('type=postbypost&limit=5'); ?>
 </ul>
 
-<h2>Archives</h2>
-<ul>
-<?php wp_get_archives('type=monthly&limit=12'); ?>
-</ul>
 
 <!--Login Script Start-->
 <?php if (!(current_user_can('level_0'))){ ?>
