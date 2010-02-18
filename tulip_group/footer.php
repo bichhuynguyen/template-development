cf71ce6eccc1b5fd72ba351f06d3c4d14107a78b
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-02-18 16:55:01 +0000

    Edit Footer

--- tulip_group/footer.php
+++ tulip_group/footer.php
@@ -1,4 +1,4 @@
-<div id="footer"><br><?php bloginfo('name'); ?> runs on <a href="http://www.wordpress.org">WordPress</a>.
+<div id="footer"><br><?php bloginfo('name'); ?> was created by <a href="http://createstudios.org.uk">Create Studios</a>.
 
 <?php wp_footer(); ?>
 
