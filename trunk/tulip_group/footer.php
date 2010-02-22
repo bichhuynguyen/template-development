a16d872748eb137f33b45378f6416bbd70bd519e
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-02-22 17:12:03 +0000

    Tweaked Login Script

--- trunk/tulip_group/footer.php
+++ trunk/tulip_group/footer.php
@@ -1,4 +1,5 @@
 <!--Login Script Start-->
+<div id="login">
 <?php if (!(current_user_can('level_0'))){ ?>
 <h2>Login</h2>
 <form action="<?php echo get_option('home'); ?>/wp-login.php" method="post">
@@ -12,10 +13,11 @@
 </form>
 <a href="<?php echo get_option('home'); ?>/wp-login.php?action=lostpassword">Recover password</a>
 <?php } else { ?>
-<h2>Logout</h2>
-<a href="<?php echo wp_logout_url(get_settings('home')); ?>">logout</a><br />
+<h2>
+<a href="<?php echo wp_logout_url(get_settings('home')); ?>">logout</a></h2><br />
 <a href="/wp-admin">admin</a>
 <?php }?>
+</div>
 <!--Login Script End-->
 
 <div id="footer"><br><?php bloginfo('name'); ?> was created by <a href="http://createstudios.org.uk">Create Studios</a>.
