7751f5ea9412db1d1ce3b86b94c935a24045a5ac
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-03-14 10:41:12 +0000

    Before Hack-Atatck

--- starkers/featured_section_backup.php
+++ starkers/featured_section_backup.php
@@ -1,37 +1,44 @@
 <div id="featured_section">
-	<div class="slide_info">
-	<h3>Our Portfolio</h3>
-	<p>Here is our lastest video. Please feel free to watch this video, this was made for stan himself, he paid us in his own semen</p>
-	</div>
+	
 	
 	<div id="container">
 		<div class="slides">
-		
+			<?php
+			    $recentPosts = new WP_Query();
+			    $recentPosts->query('showposts=5');
+			?>
+			<?php while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
 		<div>
-			<img src="<?php echo bloginfo('template_directory');?>/style/image-01.jpg" />
+			
+			
+			<div class="the_video">
+			<?php echo get_post_meta($post->ID, 'video', true)?>
+			</div>
+			<div class="body_content">
+				<a class="post_title"href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+				
+					<img src="<?php echo bloginfo('template_directory'); ?>/style/images/content_brdr.png" alt="" />
+					<p class="body_copy">
+				
+						<?php the_excerpt(); ?>
+					</p>
+					<img src="<?php echo bloginfo('template_directory'); ?>/style/images/content_brdr.png" alt="" />
+				</div>
+				
+				<span class='featured_button'>
+				<a href='#' class='previous'  title='previous'><img src="<?php echo bloginfo('template_directory'); ?>/style/images/prev_btn.png" alt="<<" /></a>
+				<a href='#' class='next'  title='next'><img src="<?php echo bloginfo('template_directory'); ?>/style/images/next_btn.png" alt=">>" /></a>
+				</span>
+				
+				
+				
 		</div>
-		<div>
-			<img src="<?php echo bloginfo('template_directory');?>/style/image-02.jpg" />
-		</div>
-		<div>
-			<img src="<?php echo bloginfo('template_directory');?>/style/image-03.jpg" />
-		</div>
-		<div>
-			<img src="<?php echo bloginfo('template_directory');?>/style/image-04.jpg" />
-		</div>
+		<?php endwhile; ?>
+		
 		
 		</div>
 		
-		<div class="featured_button">
-		<a href='#' class='previous' title='previous'>prev</a>
-		<a href='#' class='next' title='next'>next</a>	
 		</div>
 		
-	
-		
-	
-		
-		</ul>
-		
-	</div>
 </div>
+<div id="featured_base">&nbsp;</div>
