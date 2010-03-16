df920fbb621feaff4cfb5a7a9f50da00a1bb77bf
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-03-16 10:14:44 +0000

    Begin Layout work

--- trunk/TULIP2/sidebar.php
+++ trunk/TULIP2/sidebar.php
@@ -4,11 +4,12 @@
  * @subpackage Starkers
  */
 ?>
+<div id="sidebar">
 		<ul>
 			<?php 	/* Widgetized sidebar, if you have the plugin installed. */
 					if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?>
 			<li>
-				<?php get_search_form(); ?>
+				<?php// get_search_form(); ?>
 			</li>
 
 			<!-- Author information is disabled per default. Uncomment and fill in your details if you want to use it.
@@ -62,20 +63,11 @@
 		
 		<ul>
 			<?php /* If this is the frontpage */ if ( is_home() || is_page() ) { ?>
-				<?php wp_list_bookmarks(); ?>
+				<?php //wp_list_bookmarks(); ?>
 
-				<li>
-					<h2>Meta</h2>
-					<ul>
-						<?php wp_register(); ?>
-						<li><?php wp_loginout(); ?></li>
-						<li><a href="http://validator.w3.org/check/referer" title="This page validates as XHTML 1.0 Transitional">Valid <abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a></li>
-						<li><a href="http://gmpg.org/xfn/"><abbr title="XHTML Friends Network">XFN</abbr></a></li>
-						<li><a href="http://wordpress.org/" title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress</a></li>
-						<?php wp_meta(); ?>
-					</ul>
-				</li>
+				
 			<?php } ?>
 
 			<?php endif; ?>
 		</ul>
+</div>
