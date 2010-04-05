bdd0932ceb01c8272538d931babcce1e1e067aa3
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-04-05 16:22:00 +0000

    Hello World
    Width, Page Position and some styling

--- trunk/South Asia/footer.php
+++ trunk/South Asia/footer.php
@@ -4,13 +4,15 @@
  * @subpackage Starkers
  */
 ?>
-</div>
+<div class="push"></div><!--Used for keeping footer stuck to base-->
+</div><!--End container-->
+</div><!--End wrapper-->
 
 <div id="footer">
 <?php get_pagination(); ?>
 
 		<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
-		<p><?php bloginfo('name'); ?> is proudly powered by <a href="http://wordpress.org/">WordPress <?php bloginfo('version'); ?></a> <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>. <!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. --></p>
+		<p><?php bloginfo('name'); ?> is was build by <a href="http://mildfuzz.com/">Mild Fuzz</a> </p>
 		
 		<?php wp_footer(); ?>
 </div>
