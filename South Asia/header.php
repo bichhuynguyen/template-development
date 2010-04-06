ae23d1eb8935f89b04fc6368db9f92d3f04de499
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-04-06 20:54:46 +0000

    Quick Edits

--- South Asia/header.php
+++ South Asia/header.php
@@ -18,11 +18,12 @@
 	</head>
 	
 	<body <?php body_class(); ?>>
+		<h3><ul class="menu"><?php wp_list_pages('depth=1&title_li=');?></ul></h3>
 		<div class="wrapper">
 		
 		<div id="header">
 			<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
 			<h2><?php bloginfo('description'); ?></h2>
-			<h3><ul class="menu"><?php wp_list_pages('depth=1&title_li=');?></ul></h3>
+			
 		</div>
 <div id="container">
