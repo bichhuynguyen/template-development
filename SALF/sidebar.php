c0491c357b62c416707c1ce4a42dfdc9eab8557d
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-04-11 20:32:49 +0000

    Initial Setup

--- SALF/sidebar.php
+++ SALF/sidebar.php
@@ -1,4 +1,4 @@
-		<ul>
+		<ul id="sidebar">
 			<?php 	/* Widgetized sidebar, if you have the plugin installed. */
 					if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?>
 			<li>
