97bfcc84039cdcabb9f7a0f69f815f5565dceaae
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-05-26 14:28:10 +0000

    New custom post type - Events

--- salf/functions.php
+++ salf/functions.php
@@ -8,3 +8,68 @@ if ( function_exists('register_sidebar') ) {
    ));
 }
 ?>
+
+
+<?php
+add_action('init', 'my_custom_init');
+function my_custom_init() 
+{
+  $labels = array(
+    'name' => _x('Events', 'post type general name'),
+    'singular_name' => _x('Event', 'post type singular name'),
+    'add_new' => _x('Add New', 'Event'),
+    'add_new_item' => __('Add New Event'),
+    'edit_item' => __('Edit Event'),
+    'new_item' => __('New Event'),
+    'view_item' => __('View Event'),
+    'search_items' => __('Search Event'),
+    'not_found' =>  __('No Events found'),
+    'not_found_in_trash' => __('No events found in Trash'), 
+    'parent_item_colon' => ''
+  );
+	/*$taxonomies = array(
+		'tax1' => 'tax1',
+		'tax2' => 'tax2'
+		);*/
+	
+  $args = array(
+    'labels' => $labels,
+	//'taxonomies' => $taxonomies,
+    'public' => true,
+    'publicly_queryable' => true,
+    'show_ui' => true, 
+    'query_var' => true,
+    'rewrite' => true,
+    'capability_type' => 'post',
+    'hierarchical' => false,
+    'menu_position' => null,
+    'supports' => array('title','editor','author','thumbnail','excerpt','comments')
+  ); 
+  	register_post_type('event',$args);
+	register_taxonomy_for_object_type('date', 'event');
+}
+
+//add filter to insure the text Event, or Event, is displayed when user updates a Event 
+add_filter('post_updated_messages', 'Event_updated_messages');
+function Event_updated_messages( $messages ) {
+
+  $messages['Event'] = array(
+    0 => '', // Unused. Messages start at index 1.
+    1 => sprintf( __('Event updated. <a href="%s">View Event</a>'), esc_url( get_permalink($post_ID) ) ),
+    2 => __('Custom field updated.'),
+    3 => __('Custom field deleted.'),
+    4 => __('Event updated.'),
+    /* translators: %s: date and time of the revision */
+    5 => isset($_GET['revision']) ? sprintf( __('Event restored to revision from %s'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+    6 => sprintf( __('Event published. <a href="%s">View post</a>'), esc_url( get_permalink($post_ID) ) ),
+    7 => __('Event saved.'),
+    8 => sprintf( __('Event submitted. <a target="_blank" href="%s">Preview Event</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+    9 => sprintf( __('Event scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview post</a>'),
+      // translators: Publish box date format, see http://php.net/date
+      date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink($post_ID) ) ),
+    10 => sprintf( __('Event draft updated. <a target="_blank" href="%s">Preview Event</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+  );
+
+  return $messages;
+}
+?>
