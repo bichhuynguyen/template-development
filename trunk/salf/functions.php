7a93abb6ef3c5212a16755056fc58b9b091ff0dc
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-06-02 08:32:50 +0000

    new init

---
+++ trunk/salf/functions.php
@@ -0,0 +1,81 @@
+<?php
+if ( function_exists('register_sidebar') ) {
+   register_sidebar(array(
+       'before_widget' => '<li id="%1$s" class="widget %2$s">',
+       'after_widget' => '</li>',
+       'before_title' => '<h2 class="widgettitle">',
+       'after_title' => '</h2>',
+   ));
+}
+
+
+
+add_action('init', 'add_events_object');
+
+function add_events_object() 
+{
+  $labels = array(
+    'name' => _x('Events', 'post type general name'),
+    'singular_name' => _x('Event', 'post type singular name'),
+    'add_new' => _x('Add New', 'Event'),
+    'add_new_item' => __('Add New Event'),
+    'edit_item' => __('Edit Event'),
+    'new_item' => __('New Event'),
+    'view_item' => __('View Event'),
+    'search_items' => __('Search Event'),
+    'not_found' =>  __('No Events found'),
+    'not_found_in_trash' => __('No events found in Trash'), 
+    'parent_item_colon' => ''
+  );
+	/*$taxonomies = array(
+		'tax1' => 'tax1',
+		'tax2' => 'tax2'
+		);*/
+	
+  $args = array(
+    'labels' => $labels,
+	//'taxonomies' => $taxonomies,
+    'public' => true,
+    'publicly_queryable' => true,
+    'show_ui' => true, 
+    'query_var' => true,
+    'rewrite' => true,
+    'capability_type' => 'post',
+    'hierarchical' => false,
+    'menu_position' => null,
+    'supports' => array('title','editor')
+  ); 
+  	register_post_type('event',$args);
+	$date_args = array(
+	'show_ui' => true,
+	);
+	$date_labels = array(
+	'name' => 'Genres',
+	);
+	register_taxonomy('Genres', 'event');
+}
+
+//add filter to insure the text Event, or Event, is displayed when user updates a Event 
+add_filter('post_updated_messages', 'Event_updated_messages');
+function Event_updated_messages( $messages ) {
+
+  $messages['Event'] = array(
+    0 => '', // Unused. Messages start at index 1.
+    1 => sprintf( __('Event updated. <a href="%s">View Event</a>'), esc_url( get_permalink($post_ID) ) ),
+    2 => __('Custom field updated.'),
+    3 => __('Custom field deleted.'),
+    4 => __('Event updated.'),
+    /* translators: %s: date and time of the revision */
+    5 => isset($_GET['revision']) ? sprintf( __('Event restored to revision from %s'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+    6 => sprintf( __('Event published. <a href="%s">View post</a>'), esc_url( get_permalink($post_ID) ) ),
+    7 => __('Event saved.'),
+    8 => sprintf( __('Event submitted. <a target="_blank" href="%s">Preview Event</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+    9 => sprintf( __('Event scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview post</a>'),
+      // translators: Publish box date format, see http://php.net/date
+      date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink($post_ID) ) ),
+    10 => sprintf( __('Event draft updated. <a target="_blank" href="%s">Preview Event</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+  );
+
+  return $messages;
+}
+?>
---
+++ trunk/salf/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+
+	<head profile="http://gmpg.org/xfn/11">
+		
+		<title>
+			<?php bloginfo('name');
+			?>
+		</title>
+
+	    <meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
+		<meta name="description" content="<?php bloginfo('description') ?>" />
+		<?php if(is_search()) { ?>
+		<meta name="description" content="The DSC South Asian Literature Festival is the UK’s first major literature festival dedicated to the rich and varied cultures of the South Asian Subcontinent, from India and Pakistan through to Bangladesh, Sri Lanka and Nepal. It is about showcasing a rich storytelling heritage in all its forms – imagery, film, music, dance and art through the central prism of words.">
+		<meta name="keywords" content="DSC, South Asia, Asia, London, Amphora Arts, Amphora, South Asian Literature, London Literature Festival, Asian Literature, Assamese, Asomiya, Bengali, Bodo, Burmese, Dari, Dhivehi, Dogri, Dzongkha, English, Gujarati, Hindi, Kannada, Kashmiri, Konkani, Maithili, Malayalam, Marathi, Manipuri, Nepali, Oriya, Pashto, Persian, Punjabi, Sanskrit, Santhali, Sindhi, Sinhala, Siraiki, Tamil, Telugu, Tibetan, Urdu, London, Asian Books, Books, Language, Arts, Celebration, Indian Subcontinent, Bangladesh, Bhutan, India, Maldives, Nepal, Pakistan, Sri Lanka,Kings Place,Somerset House,Courtauld,Asia House,British Library,British Museum,Free Word Centre,Southbank Centre,Purcell Room,Shakespeares Globe,Rich Mix,Foyles,Waterstones,Blackwell,Aamer Hussein,Ali Sethi,Amit Choudhury,Amitava Kumar,Boria Majumdar,Chetan Bhagat,Daisy Hassan,Daljit Nagra,Daniyal Mueenuddin,Fatima Bhutto,Gautam Malkani,Geoff Dyer,Hanif Kureishi,Kamila Shamsee,Kishwar Desai,Mark Ramprakash,Meera Syal,Meghnad Desai,Mihir Bose,Moni Mohsin,Nayantara Sahgal,Nikesh Shukla,Piers Moore Ede,Rana Dasgupta,Romesh Gundasekara,Seema Anand,Sharmishta Gooptu,Shazia Omar,Siddharth Bose,Sir Mark Tully,Vayu Naidu,Vikram Seth,William Dalrymple,Booksellers Association,Booktrust,DIPNET,Livity,Live East,London Book Fair,German Book Office,Publishers Association ,Reading Agency,Society of Young Publishers,Spinebreakers,Independent Publishers Guild,Arts Council,British Council,Nehru Centre,Open University,Local Education Authorities,MLA,Museums Libraries Archives,GLA,Greater London Authority,Apples &amp; Snakes,Vayu Naidu,Company,Wasafiri,Asian Media and Marketing Group,FMcM,Fiona McMorrough,Surina Narula,Manhad Narula,DSC Prize for South Asian Literature,DSC Prize">
+	    <?php }?>
+	
+		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
+		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+		
+		
+
+		<?php wp_head(); ?>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+		<script src="<?php bloginfo('template_url'); ?>/salf_ui.js" type="text/javascript"></script>
+
+	</head>
+
+<body <?php body_class();?>>
+		<script type="text/javascript">
+		var templateDir = "<?php bloginfo('template_directory') ?>";
+		</script>
+		
+		<div id="header">
+		<h1><a href="<?php echo get_option('home'); ?>/"><span><?php bloginfo('name'); ?></span></a></h1>
+		<p><?php bloginfo('description'); ?></p>
+			
+				
+			
+			
+			
+		</div>
+		
+			
+		<div id="content-wrapper">
+			
+			<div id="nav">
+				<ul id="pages">
+
+					<li class="page_item"><a href="#post-2" title="About" id="about_btn">ABOUT</a></li>
+					<li class="page_item"><a href="#venues" title="Venues" id="venues_btn">VENUES</a></li>
+					<?php if(function_exists('add_custom_background')):?>
+					<li class="page_item"><a href="#events" title="Venues" id="events_btn">EVENTS</a></li>
+					<? endif;?>
+					<li class="page_item"><a href="#partners" title="Partners" id="partners_btn">PARTNERS</a></li>
+					<li class="page_item"><a href="#enquiries" title="Venues" id="enquiries_btn">ENQUIRIES</a></li>
+
+				</ul>	
+				
+				<?php get_sidebar(); ?>
+			</div>
