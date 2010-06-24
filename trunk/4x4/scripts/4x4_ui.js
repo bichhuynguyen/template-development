cc15d18a9d3f7ee04562842ab5b85737c7b58387
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-06-24 09:54:59 +0000

    logos

--- trunk/4x4/scripts/4x4_ui.js
+++ trunk/4x4/scripts/4x4_ui.js
@@ -1,21 +1,21 @@
 jQuery(document).ready(function($) {
 	map_show=false;
-	
+	currentMap=$('#map').css('margin-top');
 	$('#prem a').click(function(){
 		if (!map_show){
-				$('#map').slideDown(500);	
+				$('#map').animate({'margin-top':"0px"});	
 				map_show=true;	
 				
 				
 		} else {
-		$('#map').slideUp(500);	
+		$('#map').animate({'margin-top':currentMap});	
 		map_show=false;
 		}
 		return false;
 	});
 	$('#map').click(function(){
 		if(map_show){
-			$('#map').slideUp(500);	
+			$('#map').animate({'margin-top':currentMap});	
 			map_show=false;
 		}
 	});
