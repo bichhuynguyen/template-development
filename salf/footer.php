b352adf735b7dae37f5b370683e07989d4e66212
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-06-30 18:13:22 +0000

    Lots of styling.

--- salf/footer.php
+++ salf/footer.php
@@ -1,7 +1,7 @@
 	<div class="push"></div>
 	</div><!--END WRAPPER-->
 	<div id="footer">
-		<img style="padding-bottom: 5px;"src="<?php echo get_bloginfo('template_url'); ?>/style/images/ace-lottery.jpg" width="85" height="40" alt="funded by Arts Council England">
+		<img style="padding-bottom: 5px;"src="<?php echo get_bloginfo('template_url'); ?>/style/images/ace-lottery.jpg" width="85" height="40" alt="funded by Arts Council England" />
 		<p class="copyright">&#169; Amphora Arts 2010</p>
 		<?php wp_footer(); ?>
 	</div>
