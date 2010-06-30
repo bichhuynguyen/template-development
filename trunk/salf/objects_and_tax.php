08b76a3d60664c178dec65f301ee10e5b690a072
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-06-30 13:46:34 +0000

    Simplified Objects/Taxonomy in functions.php

--- trunk/salf/objects_and_tax.php
+++ trunk/salf/objects_and_tax.php
@@ -1,43 +1,4 @@
-<?php
-/*--------------
-Thumbnail Support
-----------------*/
-add_theme_support( 'post-thumbnails' );
-set_post_thumbnail_size( 100, 100, true);
-add_image_size( 'small-uncropped', 100, 100);
-add_image_size( 'small-cropped', 100, 100, true);
-add_image_size( 'med-uncropped', 200, 200);
-add_image_size( 'med-cropped', 200, 200, true);
-add_image_size( 'large-uncropped', 500, 500);
-add_image_size( 'large-cropped', 500, 500, true);
-add_image_size( 'partner-titles', 274, 41);
-
-
-function mf_post_thumbnail($style){
-if ( has_post_thumbnail() ) {
-	echo "<div class='news-thumb'>";
-	the_post_thumbnail($style);
-	echo "</div>";
-	} else {
-		/*echo "<div class='news-thumb'><img width='200' height='200'   src='";
-		echo bloginfo('template_directory'); 
-		echo"/images/teacher_no_thumb.png'></div>";*/
-	} 
-
-}
-/*--------------
-Sidebar Support
-----------------*/
-if ( function_exists('register_sidebar') ) {
-   register_sidebar(array(
-       'before_widget' => '<li id="%1$s" class="widget %2$s">',
-       'after_widget' => '</li>',
-       'before_title' => '<h2 class="widgettitle">',
-       'after_title' => '</h2>',
-   ));
-}
-
-/*
+<?php /*
 ---------------
 Objects and Tax
 ---------------
@@ -48,7 +9,7 @@ function add_fitzgraham_objects_and_taxonomy(){
 	
 	add_new_object('Partners');
 	add_new_object('People');
-	add_new_taxonomy('Partners', array('People','Partners'));
+	add_new_taxonomy('Status', array('People','Partners'));
 	
 	//add_new_object('Prices');
 }
@@ -92,11 +53,7 @@ function mf_create_labels($label){
 }
 /*
 ---------------
------END-------
+Objects and Tax
 ---------------
 */
-
-
-
-
 ?>
