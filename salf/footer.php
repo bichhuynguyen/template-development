41c2121595400753678f32bad66118516a32b4c2
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-07-14 13:44:50 +0000

    footer/venues fix

--- salf/footer.php
+++ salf/footer.php
@@ -13,16 +13,19 @@
 	</script>
 	
 
-	<small style="color: white;">Designed by <a style="color: #ddd; text-decoration: underline;" href="mailto:say@holapascal.com">Pascal Barry</a> - Developed by <a style="color: #ddd; text-decoration: underline;" href="http://mildfuzz.com">Mild Fuzz</a>
-	</small>
-	<div class="push"></div>
-	</div><!--END WRAPPER-->
+	
+	
+	
 	<div id="footer">
-		<img style="padding-bottom: 5px;"src="<?php echo get_bloginfo('template_url'); ?>/style/images/ace-lottery.jpg" width="85" height="40" alt="funded by Arts Council England" />
-		<p class="copyright">&#169; Amphora Arts 2010</p>
+		
+		<span class="copyright">
+			<small style="color: white;">Designed by <a style="color: #F7D201; text-decoration: underline;" href="mailto:say@holapascal.com">Pascal Barry</a>
+				 - Developed by <a style="color: #F7D201; text-decoration: underline;" href="http://mildfuzz.com">Mild Fuzz</a>
+	 - &#169; Amphora Arts 2010	</small></span>	
+		
 		<?php wp_footer(); ?>
 	</div>
-	
+	</div><!--END WRAPPER-->
 	
 </body>
 
