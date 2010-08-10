abca951ff135466cfe4acdbb190b18afa085db83
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-08-10 22:18:36 +0000

    Work on including date search

--- salf/sort_by_meta.php
+++ salf/sort_by_meta.php
@@ -3,6 +3,10 @@ session_start();//firephp
 //FB::log($_SESSION['venues'],'Venues');//firephp
 //FB::log($_SESSION['get_post_ID_by_meta_value'],'Venues');//firephp
 
+
+//-------------
+//Venue Sorting
+//-------------
 function mf_SALF_sort_by_meta($type){
 global $wpdb;
 $meta_array = array();
@@ -75,14 +79,123 @@ function match_venues_to_used_meta($venue_array){
 	return $final_used_venue_list;
 }
 
-
+//---------------
 //Date Sorting
+//---------------
 //Functions for sorting program info by date
 
 
-//Get Days/Month/Year Values of available program events. Return an object array.
-function get_program_dates(){
+//Creates an array with all program dates, with program post id, as multidimensional array 
+function create_all_program_dates_array(){
+	global $wpdb;
 	
+	//Build Query. Fetch all post id's and dates from Post Meta
+	$query = "SELECT post_id, meta_value FROM $wpdb->postmeta WHERE meta_key='mf_SALF_meta_date'";
+	// Run Query
+	$post_ID_query= $wpdb->get_results($query);
+	//seperate days/months/years
+	foreach ($post_ID_query as $post_dates){
+		
+		//create $post_id->$date array
+		$dates[$post_dates->post_id]=$post_dates->meta_value;
+		
+		//explode dates within array as day/month/year
+		foreach ($dates as $post_id=>$post_date){
+			$exploded_post_date_array[$post_id] = explode('/',$post_date);
+			
+			//Create Key Values, Day, Month, Year
+			$exploded_post_date_array[$post_id]['day']= $exploded_post_date_array[$post_id][0];
+			$exploded_post_date_array[$post_id]['month']= $exploded_post_date_array[$post_id][1];
+			$exploded_post_date_array[$post_id]['year']= $exploded_post_date_array[$post_id][2];
+			
+			//remove old key values
+			unset($exploded_post_date_array[$post_id][0]);
+			unset($exploded_post_date_array[$post_id][1]);
+			unset($exploded_post_date_array[$post_id][2]);
+		}
+	
+	}
+	
+	
+	return $exploded_post_date_array;
 }
+
+//create an array containing all dates in a month, with post id's 
+//attached to dates, and FALSE on days with no date.
+
+
+function create_calendar_array($used_dates){
+
+	//create array of dates for October with false attributes
+	$october = array();
+	for($day=1;$day<=31;$day++){
+		$october[$day]=FALSE;
+	}
+	
+	//compare used dates to whole month
+	foreach	($used_dates as $post_id=>$date){
+		$october[intval($date['day'])][]=$post_id;
+	}
+		
+	
+	return $october;
+}
+
+// creates grid calendar
+function create_html_calendar($october){
+	//create HTML output
+	$html_output = '<table id="calendar_search">';
+	$html_output .= '<thead>';
+	$html_output .= '<tr>';
+	$html_output .= '<th class="weekday" scope="col" abbr="Monday" title="Monday">M</th>';
+	$html_output .= '<th class="weekday" scope="col" abbr="Tuesday" title="Tuesday">T</th>';
+	$html_output .= '<th class="weekday" scope="col" abbr="Wednesday" title="Wednesday">W</th>';
+	$html_output .= '<th class="weekday" scope="col" abbr="Thursday" title="Thursday">T</th>';
+	$html_output .= '<th class="weekday" scope="col" abbr="Friday" title="Friday">F</th>';
+	$html_output .= '<th class="weekday" scope="col" abbr="Saturday" title="Saturday">S</th>';
+	$html_output .= '<th class="weekday" scope="col" abbr="Sunday" title="Sunday">S</th>';
+	$html_output .= '</tr>';
+	$html_output .= '</thead>';
+	
+	$html_output .= '<tbody>';
+	
+	$weekday = 4;//Offset to start month on FRIDAY.
+	$html_output .= "<tr><td></td><td></td><td></td><td></td>";
+	$firstweek = TRUE;
+	$_SESSION['date_process_url'] = curPageURL();//Set Current Location for form processing
+	foreach ($october as $id => $post){
+		if ($weekday == 0){
+			$html_output .= "<tr>";
+		} 
+		
+			if($post){
+				//create string for all posts in this section
+				$posts_string = NULL;
+				foreach($post as $post_id){
+					if ($posts_string == NULL){
+						$posts_string .= $post_id;
+					} else {$posts_string .=','.$post_id;}
+				}
+				$html_output .= "<td class='has_posts'><a href='".get_bloginfo('template_url')."/date_processing.php?posts=".$posts_string."'>".$id."</a></td>";
+			} else{
+				$html_output .= "<td class='no_posts'>".$id."</td>";
+			}
+		
+		$weekday++;
+		if ($weekday == 7){
+			$html_output .= '</tr>';
+			$weekday = 0;
+			$firstweek = FALSE;
+		} 
+		
+	}
+	
+	
+	$html_output .= '</tbody>';
+	$html_output .= '</table>';
+	
+	return $html_output;
+}	
+	
 
 ?>
