420e02264774c20a2017447b50086c6d1394bbdf
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-08-11 16:04:54 +0000

    Implement $_SESSION variable for venues during date processing.

--- trunk/salf/date_processing.php
+++ trunk/salf/date_processing.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$_SESSION['date_search']=TRUE;//tells program page session has ran.
 $_SESSION['date_posts']=$_GET['posts'];
 
 header('Location: '.$_SESSION['date_process_url']);
