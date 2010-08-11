b8f01f6a94722d02a3f868a39d59d4d8fba877a3
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-08-11 17:04:17 +0000

    Date Search Objects Returned

--- trunk/salf/date_processing.php
+++ trunk/salf/date_processing.php
@@ -1,7 +1,19 @@
 <?php
 session_start();
+
 $_SESSION['date_search']=TRUE;//tells program page session has ran.
-$_SESSION['date_posts']=$_GET['posts'];
+
+//send object array back filled with posts
+$selected_date_post_ids = explode(',',$_GET['posts']);//make array from get variables (send by URL's in calendar links)
+
+//convert strings in array to integers
+foreach ($selected_date_post_ids as $string_to_int){
+	$processed_selected_date_post_ids[] = intval($string_to_int);
+}
+
+
+
+$_SESSION['date_posts']=$processed_selected_date_post_ids;
 
 header('Location: '.$_SESSION['date_process_url']);
 
