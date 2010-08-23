4c435d4969f7515b5a4d8dab1a02658184818094
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-08-23 20:29:47 +0000

    No event bug fixed

--- trunk/salf/holding-page.php
+++ trunk/salf/holding-page.php
@@ -176,15 +176,18 @@ Template Name: Holding Page
 			query_posts( array( 'post_type' => 'Venues') );
 			
 			if ( have_posts() ) : while ( have_posts() ) : the_post();?>
+			
 			<div class="venue-box">
+				<a class='venue' href="<? the_permalink();?>">
 			<?php
 			if ( has_post_thumbnail() ) {
 				the_post_thumbnail('venue-images');
 			} 
 			?>
 			<p><?php the_title();?></p>
-			
+			</a>
 			</div>
+			
 			<?php endwhile; else:?>
 			 
 			<?php endif;
