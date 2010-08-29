fbb86926daa183e63436eebb92510e8b98d32e96
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-08-29 15:12:14 +0000

    Seperate Pages

---
+++ salf/events.php
@@ -0,0 +1,38 @@
+<?php
+ob_start();
+
+/*
+Template Name: Events Page
+*/
+?>
+<?php get_header(); ?>
+
+		
+		<div id="events" class="post">
+			
+			<img src="<?php echo bloginfo('template_url'); ?>/style/images/Events-title.png" alt="Events Title">
+			<div id="event-type-boxes">
+			<?php
+			query_posts( array( 'post_type' => 'Events') );
+			
+			if ( have_posts() ) : while ( have_posts() ) : the_post();?>
+			<div class="event-type-box">
+			<?php
+			if ( has_post_thumbnail() ) {
+				the_post_thumbnail();
+			} 
+			?>
+			<h3><?php the_title();?>	</h3>
+			<p style="float: right"><?php the_content();?></p>
+			</div>
+			<?php endwhile; else:?>
+			 
+			<?php endif;
+			//Reset Query
+			wp_reset_query();
+			?>
+			</div>
+		</div>
+		
+		
+<?php get_footer(); ?>
