94604a43b65fbd268b48d532bec7426cb0050310
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-08-31 20:51:14 +0000

    major update to program look and feel

--- trunk/salf/scripts/program.js
+++ trunk/salf/scripts/program.js
@@ -9,20 +9,24 @@ jQuery(document).ready(function($) {
 	current_header_width = $('#program_feed .event h3').css('width');
 	$('#program_feed .event h3').css('width', '70%');
 	clicked = false
-	$(program_titles).append("<span style='display: inline;color: #D92417;' class='action-call'>more</span>");
+	$(program_titles).append("<span class='action-call'>more</span>");
 	
 	//show/hide function
 	$('#program_feed .event').click(function(){
 		if (clicked != this){
 			clicked = this;
 			program_elements.hide();
+			$('#program_feed .event').removeClass('active');
+			$(this).addClass('active');
 			$('#program_feed .event h3').css('width', '70%');
+			$('#program_feed .event h3 span').not($(this).children('h3').children('span')).html('more');
 			$(this).children('h3').children('span').html('less');
-			$(this).children('div').add($(this).children('p')).fadeIn();
+			$(this).children('div').add($(this).children('p')).slideDown();
 		} else {
 			clicked = false;
+			$(this).removeClass('active');
 			$(this).children('h3').children('span').html('more');
-			$(this).children('div').add($(this).children('p')).hide();
+			$(this).children('div').add($(this).children('p')).slideUp();
 		}
 	});
 });
