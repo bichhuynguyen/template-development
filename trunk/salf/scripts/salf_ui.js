149183ea2d556182205d2ea70797e5321e101b26
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-09-01 13:26:49 +0000

    News Javascript live

--- trunk/salf/scripts/salf_ui.js
+++ trunk/salf/scripts/salf_ui.js
@@ -225,7 +225,8 @@ jQuery(document).ready(function($) {
 		$('#main-content-area .post').animate({opacity:0.1},500,function(){
 			$(this).children().remove();
 			$(this).parent().load(to_load, function() {
-				program_js();
+				program_js();//scripts needed to run pages
+				archives_js();
 			  	hash = window.location.hash;
 				current_id = $('.post').attr('id');
 				$('.post').addClass(current_id);
@@ -268,6 +269,49 @@ jQuery(document).ready(function($) {
 			}
 		});
 	};
+	function archives_js(){
+	news = $('.archive-page .new-entry');
+	news.each(function(){
+   			grandparent = $(this).parent().parent();
+        	href = $(this).parent('a').attr('href');
+			$(this).append('<a href="'+href+'">Read More</a>');
+         	object = $(this).parent().html();
+			grandparent.prepend(object);
+         	$(this).parent().remove();
+        
+        
+    })
+    news = $('.archive-page .new-entry');
+
+	news_titles = $('.archive-page .new-entry h2').add('post-details');
+	news_elements = $('.archive-page .new-entry').children().not(news_titles);
+	news_elements.hide();
+	
+
+	news.css({'min-height':'80px'});
+	
+	clicked = false
+	$(news_titles).append("<span class='action-call'>more</span>");
+	
+	//show/hide function
+	news.click(function(){
+		if (clicked != this){
+			clicked = this;
+			news_elements.hide();
+			news.removeClass('active');
+			$(this).addClass('active');
+			$(this).children().slideDown();
+			news.children('span').not($(this).children('span')).html('more');
+			$(this).children('span').html('less');
+		} else {
+			clicked = false;
+			$(this).removeClass('active');
+			$(this).children('h3').children('span').html('more');
+			$(this).children().not('h2').slideUp();
+			$(this).children('span').html('more');
+		}
+	});
+	}
 	/*----------------------------------------------------------------
 	------------------END AJAX NAVIGATION
 	//----------------------------------------------------------------*/
