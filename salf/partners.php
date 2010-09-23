db8650fdccc464d294572b388cae92aca0ebdb2a
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-09-23 20:05:15 +0000

    Make Partners backend manageable.

--- salf/partners.php
+++ salf/partners.php
@@ -11,34 +11,16 @@ Template Name: Partners Page
 	<div class="post partners">		
 		<h2><img class="text" src="<?php echo bloginfo('template_url'); ?>/style/images/partners-blurb.png" alt="the list of partners continues to grow" ></img></h2>
 		
-		<img class="logos" src="<?php echo bloginfo('template_url'); ?>/style/images/partners-logo.png" alt="Partners Logo" ></img>
 		
-		<ul id="partner-links">
-			<li><a href="http://www.foyles.co.uk/">Foyles</a></li>
-			<li><a href="http://www.ipg.uk.com/">IPG</a></li>
-			<li><a href="http://www.britishcouncil.org">British Council</a></li>
-			<li><a href="http://www.applesandsnakes.org/">Apples &amp; Snakes</a></li>
-			<li><a href="http://www.open.ac.uk/">The Open University</a></li>
-			<li><a href="http://www.amg.biz/">AMG</a></li>
-			<li><a href="http://www.nehrucentre.org.uk/">The Nehru Centre</a></li>
-			<li><a href="http://www.readingagency.org.uk/">The Reading Agency</li>
-			<li><a href="http://www.freewordonline.com/">Free Word Centre</a></li>
-			<li><a href="http://www.publishers.org.uk/en/home/">The Publishers Association</a></li>
-			<li><a href="http://www.wasafiri.org/">Wasafiri</a></li>
-			<li><a href="http://www.bl.uk/">British Library</a></li>
-			<li><a href="http://www.granta.com/">Granta</a></li>
-			<li><a href="http://www.vayunaiducompany.org.uk/">Vaya Naidu Company</a></li>
-			<li><a href="http://www.spinebreakers.co.uk/Pages/Home.aspx">Spine Breakers</a></li>
-			<li><a href="http://www.richmix.org.uk/">Rich Mix</a></li>
-			<li><a href="http://www.dipnet.org.uk/">DIPNET</a></li>
-			<li><a href="http://www.kingsplace.co.uk/">Kings Place</a></li>
-			<li><a href="http://www.artscouncil.org.uk/">Arts Council England</a></li>
-			<li><a href="http://www.livity.co.uk/">Livity</a></li>
-			<li><a href="http://www.booksellers.org.uk/">The Booksellers Association</a></li>
-			<li><a href="http://www.cii.in/">CII</a></li>
-			<li><a href="http://www.saadhak.co.uk/">Saadhak</a></li>
-			<li><a href="http://www.thesyp.org.uk/">SYP</a></li>
-		</ul>
+		
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php
+			if ( has_post_thumbnail() ) {
+				the_post_thumbnail('partners-image');
+			} 
+			?>
+		<?php the_content(); ?>
+		<?php endwhile; endif; ?>
 		
 	</div>
 		
