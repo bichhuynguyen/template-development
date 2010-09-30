d2b7f425f07d3e5004de50ec7886a0214aac5c0e
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-09-30 12:19:58 +0000

    .home h2 height fix read more hover added

--- trunk/salf/front_page_loop.php
+++ trunk/salf/front_page_loop.php
@@ -1,5 +1,5 @@
 <div class="new-entry">
-						<a href="<?php the_permalink();?>"><h2><?php echo get_the_title(); ?></h2></a><div class="post-details"><?php the_time('l, F jS, Y') ?><?php comments_number('',' - 1 comment',' - % comments'); ?></div>
+						<a href="<?php the_permalink();?>" title="Read More..."><h2><?php echo get_the_title(); ?></h2></a><div class="post-details"><?php the_time('l, F jS, Y') ?><?php comments_number('',' - 1 comment',' - % comments'); ?></div>
 						<?php mf_post_thumbnail();?>
 						<?php 
 						global $more;    // Declare global $more (before the loop).
