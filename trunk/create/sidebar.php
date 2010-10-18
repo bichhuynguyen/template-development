aff9bdc726a97f4e72fd5562b5191ebf54ba54ca
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-10-18 08:56:07 +0000

    Next & Previous Buttons

--- trunk/create/sidebar.php
+++ trunk/create/sidebar.php
@@ -44,10 +44,11 @@
 							$player = $side_vid->get_requested_video($first_video['first_film_id']);
 							echo '<h4>'.$player['title'].'</h4>';
 							
-							echo $player['prev'];
-							echo $player['next'];
 							
+							echo "<p class='video-nav'>Next Video:".$player['next']."</p>";
+							echo "<p class='video-nav'>Previous Video:".$player['prev']."</p>";
 							echo $player['video'];
+							
 							echo '<p>'.$player['desc'].'</p>';?>
 							</div>
 							</li>
