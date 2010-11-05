2ea0ad5130fdac261a6db07a70d07a964f9bc2df
johnfarrow82 <johnfarrow82@032bc016-1195-11df-b47a-af0d17c1a499>
2010-11-05 11:24:57 +0000

    object class defined and working

--- trunk/create/functions/objects_class.php
+++ trunk/create/functions/objects_class.php
@@ -1,41 +1,55 @@
 <?php
 class optionObject{
-	var $option_group ="extra_options";//Group Slug for settings whitelist
-	var $option_prefix = "extra_";//name prefix set
-	var $page_title = "Extra Settings";//Page Title
-	var $menu_page_title = "Extra Settings";//Settings Page Name for sidemenu
-	var $user_level = 'manage_options';//set minimum user access level
-	var $menu_slug = "extra_settings";//ID for top level menu
-	var $options = array(
-						array("title"=>"Extra 1","slug"=>"extra1","help"=>"A little extra something")
-						);
+	var $args = array(
+			'option_group'		=> "extra_options",		//Group Slug for settings whitelist	
+			'option_prefix'		=> "extra_",			//name prefix set
+			'page_title'		=> "Extra Settings",	//Page Title
+			'menu_page_title' 	=> "Extra Settings",	//Settings Page Name for sidemenu
+			'user_level' 		=> 'manage_options',	//set minimum user access level
+			'menu_slug' 		=> "extra_settings",	//ID for top level menu
+			'options'			=> array(
+								array("title"=>"Extra 1","slug"=>"extra1","help"=>"A little extra something"))
+								
 						/*
 						* Multi Dimensional Array, requires title, slug and help (all strings) for each
 						* required option 	
 						*/
-		
+			);//End of Args Defintion
 	
-	function __construct($option_group = false, $option_prefix = false){
+	
+	
+	
+	
+	
+	
+	
+
+	
+	function __construct($args = false){
 		fb::log("constructed");
-		if(!$option_group) $option_group = $this->option_group;
-		$this->option_group =$option_group;
-		if(!$option_prefix) $option_prefix = $this->option_prefix;
-		$this->option_prefix =$option_prefix;
+		if($args && is_array($args)){
+			foreach ($args as $k=>$v){
+				$this->args[$k]= $args[$k];
+			}
+		}
 		
 		
-		register_setting( $this->option_group, $this->option_prefix, array(&$this, 'validate') );
-		
+		register_setting( $this->args['option_group'], $this->args['option_prefix'], array(&$this, 'validate') );
+		//add_action('admin_init', array(&$this, 'add_page'));
+		add_options_page($this->args['menu_page_title'], $this->args['page_title'], $this->args['user_level'], $this->args['menu_slug'], array(&$this, 'do_page'));
+
+	
 	}
 	
 	function init(){
 
-	    register_setting( $this->option_group, $this->option_prefix, array(&$this, 'validate') );
+	    register_setting( $this->args['option_group'], $this->args['option_prefix'], array(&$this, 'validate') );
 		
 	}
 
 	function add_page() {
-		fb::log('added page');
-	    add_options_page($this->menu_page_title, $this->page_title, $this->user_level, $this->menu_slug, array(&$this, 'do_page'));
+		
+	    add_options_page($this->args['menu_page_title'], $this->args['page_title'], $this->args['user_level'], $this->args['menu_slug'], array(&$this, 'do_page'));
 
 	}
 
@@ -45,31 +59,32 @@ class optionObject{
 		* required option 	
 		*/
 		
-		if(!$options) $options = $this->options;
+		if(!$options) $options = $this->args['options'];
 		
 		
 		foreach ($options as $option):?>
 			<tr valign="top"><th scope="row"><?php echo $option['title']; ?></th>
 
-                <td><input type="text" name=<?php echo $this->option_name.'['.$option['slug'].']'?> value="<?php echo $options['text']; ?>" /><p><?php echo $option[$option['slug']]; ?></p></td>
+                <td><input type="text" name=<?php echo $this->args['option_name'].'['.$option['slug'].']'?> value="<?php echo $options['text']; ?>" /><p><?php echo $option[$option['slug']]; ?></p></td>
 
             </tr>	
 		<?php endforeach;
 	}
 
 	function do_page() {
+		fb::log("Do Page launched");
 
 	    ?>
 
 	    <div class="wrap">
 
-	        <h2><?php echo $this->page_title;?></h2>
+	        <h2><?php echo $this->args['page_title'];?></h2>
 
 	        <form method="post" action="options.php">
 
-	            <?php settings_fields($this->option_group); ?>
+	            <?php settings_fields($this->args['option_group']); ?>
 
-	            <?php $options = get_option($this->option_name); ?>
+	            <?php $options = get_option($this->args['option_name']); ?>
 
 	            <table class="form-table">
 
@@ -110,22 +125,22 @@ class optionObject{
 
 
 function test_options(){
-	$options = new optionObject("test_options","test_");
-	$options->user_level = "read";
-	$options->page_title = "Test Settings";
-	$options->menu_page_title = $options->page_title;
-	$options->menu_slug = "test_settings";
-	$options->options = array(
+	
+	//$options->user_level = "read";
+	$args['page_title'] = "Test Options";
+	$args['menu_page_title'] = "Test Settings";
+	$args['menu_slug'] = "test_settings";
+	$args['options'] = array(
 								array("title"=>"Test 1","slug"=>"test1","help"=>"Help with Test 1"),
 								array("title"=>"Test 2","slug"=>"test2","help"=>"Help with Test 2")
 	);
+	$options = new optionObject($args);
 	
-	$options->init();
-	fb::log($options->option_group);
 	
-	add_action('admin_menu', array(&$options, 'add_page'));
+	
+	
 }
-add_action('admin_init', 'test_options' );
+add_action('admin_menu', 'test_options' );
 
 
 
